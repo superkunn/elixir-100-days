795cb6e3003030a2f6b926beaddde22b99b7e06a
superkunn <1150527505@qq.com>
2017-08-11 21:48:35 +0800

    udemy

---
+++ Card/card/lib/card.ex
@@ -0,0 +1,24 @@
+defmodule Card do
+  def create_deck do
+    values = ["Ace","two","three","four","five"]
+    suits = ["Hearts", "Clubs", "Spades", "Diamonds"]
+    
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+    
+
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+  
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+end
