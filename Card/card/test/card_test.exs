49612c220373cf691d4d2cfb4c9b6e40928ab1b9
superkunn <1150527505@qq.com>
2017-08-13 16:15:01 +0800

    udemy

--- Card/card/test/card_test.exs
+++ Card/card/test/card_test.exs
@@ -1,8 +1,13 @@
 defmodule CardTest do
   use ExUnit.Case
   doctest Card
+  test "create_deck makes 20 cards" do
+    deck_length = length(Card.create_deck)
+    assert deck_length == 20
+  end
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "shuffling a deck randomizes it" do
+    deck = Card.create_deck
+    refute deck == Card.shuffle(deck)
   end
 end
